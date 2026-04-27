d63802501bfea5566f534f99d5ebeb4266751d81
Sarai Ramirez <sarairace@unisabana.edu.co>
2026-04-27 17:39:05 -0500

    lab8

--- question1.py
+++ question1.py
@@ -3,4 +3,15 @@
 Implementa una CLI que calcule carga por punto de soporte.
 """
 
-# TODO: Implementar según README.md
+import sys
+
+try:
+    total = float(sys.argv[1])
+    supports = float(sys.argv[2])
+
+    result = total / supports
+
+    print("Load per support point:", round(result, 2), "N")
+
+except:
+    print("Error: Invalid input!")
