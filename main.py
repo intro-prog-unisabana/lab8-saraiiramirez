540f0a03683e457a31516662c6d822606474450f
Sarai Ramirez <sarairace@unisabana.edu.co>
2026-04-27 18:07:33 -0500

    lab 8

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 """Laboratorio 8 - CLI del gestor de tareas."""
 
-# TODO: Implementar CLI según README.md
+import sys
+from todo_manager import read_todo_file
+
+try:
+    if len(sys.argv) < 2:
+        raise IndexError("Insufficient arguments provided!")
+
+    print("Command-line arguments:")
+    for arg in sys.argv[1:]:
+        print(arg)
+
+    tasks = read_todo_file(sys.argv[1])
+
+    print("\nTasks:")
+    for task in tasks:
+        print(task)
+
+except IndexError as e:
+    print(e)
